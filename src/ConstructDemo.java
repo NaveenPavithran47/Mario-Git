5ec51abbde455b2fb9a1614136393fb3db739202
Naveen Pavithran <naveenpavithran49@gmail.com>
2021-12-13 02:34:41 +0530

    Commit from UserA from India

--- src/ConstructDemo.java
+++ src/ConstructDemo.java
@@ -4,6 +4,7 @@ public class ConstructDemo {
 	public ConstructDemo()
 	{
 		System.out.println("I'm a constructor");
+		System.out.println("I'm a git Mario constructor");
 	}
 	
 	public static void main(String[] args) {
