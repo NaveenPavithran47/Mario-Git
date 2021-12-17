442c9caa5783ff414bced3e96ae041baed96140e
Naveen Pavithran <naveenpavithran49@gmail.com>
2021-12-17 22:17:30 +0530

    Naveen

--- src/ConstructDemo.java
+++ src/ConstructDemo.java
@@ -4,7 +4,7 @@ public class ConstructDemo {
 	public ConstructDemo()
 	{
 		System.out.println("I'm a constructor");
-		System.out.println("I'm a git Mario constructor");
+		System.out.println("I'm a git Mario constructor1233");
 	}
 	
 	public static void main(String[] args) {
